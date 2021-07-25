f421332859472ce7f25e0f0bedb60ea303b4b4f2
Emmanouil Gimissis <manolis@pgimisis.gr>
2021-07-25 12:34:03 +0300

    Break into 3 possible outcomes

--- ApplicationProcessor/Application.cs
+++ ApplicationProcessor/Application.cs
@@ -39,31 +39,23 @@ namespace ULaw.ApplicationProcessor
 
             if (DegreeGrade == DegreeGradeEnum.twoTwo)
             {
-                result.Append(string.Format("<p/> Further to your recent application for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
-                result.Append("<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.");
+                result.Append(HTMLProcessing());
             }
             else
             {
                 if (DegreeGrade == DegreeGradeEnum.third)
                 {
-                    result.Append("<p/> Further to your recent application, we are sorry to inform you that you have not been successful on this occasion.");
-                    result.Append("<br/> If you wish to discuss the decision further, or discuss the possibility of applying for an alternative course with us, please contact us at AdmissionsTeam@Ulaw.co.uk.");
+                    result.Append(HTMLApplicationRejected());
                 }
                 else
                 {
                     if (DegreeSubject == DegreeSubjectEnum.law || DegreeSubject == DegreeSubjectEnum.lawAndBusiness)
                     {
-                        decimal depositAmount = 350.00M;
-
-                        result.Append(string.Format("<p/> Further to your recent application, we are delighted to offer you a place on our course reference: {0} starting on {1}.", this.CourseCode, this.StartDate.ToLongDateString()));
-                        result.Append(string.Format("<br/> This offer will be subject to evidence of your qualifying {0} degree at grade: {1}.", DegreeSubject.ToDescription(), DegreeGrade.ToDescription()));
-                        result.Append(string.Format("<br/> Please contact us as soon as possible to confirm your acceptance of your place and arrange payment of the £{0} deposit fee to secure your place.", depositAmount.ToString()));
-                        result.Append(string.Format("<br/> We look forward to welcoming you to the University,"));
+                        result.Append(HTMLApplicationAccepted());
                     }
                     else
                     {
-                        result.Append(string.Format("<p/> Further to your recent application for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
-                        result.Append("<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.");
+                        result.Append(HTMLProcessing());
                     }
                 }
             }
@@ -75,6 +67,30 @@ namespace ULaw.ApplicationProcessor
             return result.ToString();
         }
 
+        //HTML we are processing
+        public string HTMLProcessing()
+        {
+            return $"<p/> Further to your recent application for our course reference: {CourseCode} starting on {StartDate.ToLongDateString()}, we are writing to inform you that we are currently assessing your information and will be in touch shortly." +
+                   $"<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.";
+        }
+
+        //HTML application accepted
+        public string HTMLApplicationAccepted()
+        {
+            decimal depositAmount = 350.00M;
+
+            return $"<p/> Further to your recent application, we are delighted to offer you a place on our course reference: {CourseCode} starting on {StartDate.ToLongDateString()}." +
+                   $"<br/> This offer will be subject to evidence of your qualifying {DegreeSubject.ToDescription()} degree at grade: {DegreeGrade.ToDescription()}." +
+                   $"<br/> Please contact us as soon as possible to confirm your acceptance of your place and arrange payment of the £{depositAmount.ToString()} deposit fee to secure your place." +
+                    "<br/> We look forward to welcoming you to the University,";
+        }
+
+        //HTML application rejected
+        public string HTMLApplicationRejected()
+        {
+            return "<p/> Further to your recent application, we are sorry to inform you that you have not been successful on this occasion." +
+                   $"<br/> If you wish to discuss the decision further, or discuss the possibility of applying for an alternative course with us, please contact us at AdmissionsTeam@Ulaw.co.uk.";
+        }
     }
 }
 
