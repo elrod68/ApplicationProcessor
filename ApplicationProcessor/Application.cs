df3a9ec189e8e1e1644a09cabdb63432a9e774b4
Emmanouil Gimissis <manolis@pgimisis.gr>
2021-07-25 12:42:10 +0300

    Break some HTML details to primitives

--- ApplicationProcessor/Application.cs
+++ ApplicationProcessor/Application.cs
@@ -34,8 +34,8 @@ namespace ULaw.ApplicationProcessor
 
         public string Process()
         {
-            var result = new StringBuilder("<html><body><h1>Your Recent Application from the University of Law</h1>");
-            result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
+            var result = new StringBuilder(HTMLBodyStart());
+            result.Append(HTMLSalutation());
 
             if (DegreeGrade == DegreeGradeEnum.twoTwo)
             {
@@ -60,9 +60,8 @@ namespace ULaw.ApplicationProcessor
                 }
             }
 
-            result.Append("<br/> Yours sincerely,");
-            result.Append("<p/> The Admissions Team,");
-            result.Append(string.Format("</body></html>"));
+            result.Append(HTMLYours());
+            result.Append(HTMLBodyEnd());
 
             return result.ToString();
         }
@@ -71,17 +70,15 @@ namespace ULaw.ApplicationProcessor
         public string HTMLProcessing()
         {
             return $"<p/> Further to your recent application for our course reference: {CourseCode} starting on {StartDate.ToLongDateString()}, we are writing to inform you that we are currently assessing your information and will be in touch shortly." +
-                   $"<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.";
+                   $"<br/> If you wish to discuss any aspect of your application, please contact us at {AdmissionsEmail()}.";
         }
 
         //HTML application accepted
         public string HTMLApplicationAccepted()
         {
-            decimal depositAmount = 350.00M;
-
             return $"<p/> Further to your recent application, we are delighted to offer you a place on our course reference: {CourseCode} starting on {StartDate.ToLongDateString()}." +
                    $"<br/> This offer will be subject to evidence of your qualifying {DegreeSubject.ToDescription()} degree at grade: {DegreeGrade.ToDescription()}." +
-                   $"<br/> Please contact us as soon as possible to confirm your acceptance of your place and arrange payment of the £{depositAmount.ToString()} deposit fee to secure your place." +
+                   $"<br/> Please contact us as soon as possible to confirm your acceptance of your place and arrange payment of the £{DepositAmount().ToString()} deposit fee to secure your place." +
                     "<br/> We look forward to welcoming you to the University,";
         }
 
@@ -89,7 +86,44 @@ namespace ULaw.ApplicationProcessor
         public string HTMLApplicationRejected()
         {
             return "<p/> Further to your recent application, we are sorry to inform you that you have not been successful on this occasion." +
-                   $"<br/> If you wish to discuss the decision further, or discuss the possibility of applying for an alternative course with us, please contact us at AdmissionsTeam@Ulaw.co.uk.";
+                   $"<br/> If you wish to discuss the decision further, or discuss the possibility of applying for an alternative course with us, please contact us at {AdmissionsEmail()}.";
+        }
+
+        public string AdmissionsEmail()
+        {
+            //Might expand this to include different emails depending on the application data
+            return "AdmissionsTeam@Ulaw.co.uk";
+        }
+
+        public decimal DepositAmount()
+        {
+            //Might expand this to calculate different amounts depending on the application data, year or do a query in a database etc.
+            return 350.00M;
+        }
+
+        //primitive for HTML body start
+        public string HTMLBodyStart()
+        {
+            return "<html><body><h1>Your Recent Application from the University of Law</h1>";
+        }
+
+        //primitive for HTML salutation
+        public string HTMLSalutation()
+        {
+            return $"<p> Dear {FirstName}, </p>";
+        }
+
+        //primitive for HTML body end
+        public string HTMLBodyEnd()
+        {
+            return "</body></html>";
+        }
+
+        //primitive for HTML yours
+        public string HTMLYours()
+        {
+            return "<br/> Yours sincerely," +
+                   "<p/> The Admissions Team,";
         }
     }
 }
